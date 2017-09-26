0e7fc4e1ff14932881d81c252a9e1458a1dec238
Michael Schroeder <Michael.Schroeder@pmone.com>
2017-09-26 04:23:25 +0200

    Added initial test setup with mockserver

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -2,6 +2,9 @@
 /*jslint node: true */
 var expect = require('chai').expect;
 var setup  = require(__dirname + '/lib/setup');
+//const nock = require('nock');
+const mockserver = require('mockserver-grunt');
+const mockServerClient = require('mockserver-client').mockServerClient;
 
 var objects = null;
 var states  = null;
@@ -10,6 +13,155 @@ var onObjectChanged = null;
 var sendToID = 1;
 
 var adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.')+1);
+
+// Test data
+const testURL = 'http://localhost:1080';
+const authAPI = '/api/v1/auth';
+const productsAPI = '/api/v1/products';
+const scenesAPI = '/api/v1/scenes';
+const testValidPW = 'velux123';
+const testToken = '/WGoAkAwn1fSOV04E2QBRA==';
+const actionLogin = 'login';
+const actionLogout = 'logout';
+const actionProductsGet = 'get';
+const actionScenesGet = 'get';
+const loginRequest = { action: actionLogin, params: { password: testValidPW } };
+const logoutRequest = {
+    action: actionLogout, params: {}
+};
+const productsGetRequest = { action: actionProductsGet, params: {} };
+const productsGetResponse = {
+    token: testToken,
+    result: true,
+    "deviceStatus": "IDLE",
+    "data": [
+        {
+            "name": "Windows bathroom",
+            "category": "Window opener",
+            "id": 0,
+            "typeId": 4,
+            "subtype": 1,
+            "scenes": [
+                "Some windows"
+            ]
+        },
+        {
+            "name": "Window sleeping room",
+            "category": "Window opener",
+            "id": 1,
+            "typeId": 4,
+            "subtype": 1,
+            "scenes": [
+                "Some windows",
+                "Window sleeping room 0%",
+                "Window sleeping room 100%"
+            ]
+        },
+        {
+            "name": "Window kids room",
+            "category": "Window opener",
+            "id": 2,
+            "typeId": 4,
+            "subtype": 1,
+            "scenes": []
+        },
+        {
+            "name": "Roller shutter sleeping room",
+            "category": "Roller shutter",
+            "id": 3,
+            "typeId": 2,
+            "subtype": 0,
+            "scenes": []
+        },
+        {
+            "name": "Window kitchen",
+            "category": "Window opener",
+            "id": 4,
+            "typeId": 4,
+            "subtype": 1,
+            "scenes": [
+                "Window kitchen 10%",
+                "Window kitchen 20%"
+            ]
+        }
+    ],
+    "errors": []
+};
+
+const scenesGetRequest = { action: actionScenesGet, params: {} };
+const scenesGetResponse = {
+    "token": testToken,
+    "result": true,
+    "deviceStatus": "IDLE",
+    "data": [{
+        "name": "Window kitchen 10%",
+        "id": 0,
+        "silent": false,
+        "products": [{
+            "typeId": 4,
+            "name": "Window kitchen",
+            "actuator": 0,
+            "status": 10
+        }
+        ]
+    },
+    {
+        "name": "Window kitchen 20%",
+        "id": 1,
+        "silent": false,
+        "products": [{
+            "typeId": 4,
+            "name": "Window kitchen",
+            "actuator": 0,
+            "status": 20
+        }
+        ]
+    },
+    {
+        "name": "Window sleeping room 0%",
+        "id": 2,
+        "silent": true,
+        "products": [{
+            "typeId": 4,
+            "name": "Window sleeping room",
+            "actuator": 0,
+            "status": 0
+        }
+        ]
+    },
+    {
+        "name": "Window sleeping room 100%",
+        "id": 3,
+        "silent": true,
+        "products": [{
+            "typeId": 4,
+            "name": "Window sleeping room",
+            "actuator": 0,
+            "status": 100
+        }
+        ]
+    },
+    {
+        "name": "Some windows",
+        "id": 4,
+        "silent": false,
+        "products": [{
+            "typeId": 4,
+            "name": "Windows bathroom",
+            "actuator": 0,
+            "status": 50
+        },
+        {
+            "typeId": 4,
+            "name": "Window sleeping room",
+            "actuator": 0,
+            "status": 50
+        }
+        ]
+    }
+    ],
+    "errors": []
+};
 
 function checkConnectionOfAdapter(cb, counter) {
     counter = counter || 0;
@@ -73,28 +225,133 @@ function sendTo(target, command, message, callback) {
     });
 }
 
+let scope;
+
 describe('Test ' + adapterShortName + ' adapter', function() {
     before('Test ' + adapterShortName + ' adapter: Start js-controller', function (_done) {
         this.timeout(600000); // because of first install from npm
 
-        setup.setupController(function () {
-            var config = setup.getAdapterConfig();
-            // enable adapter
-            config.common.enabled  = true;
-            config.common.loglevel = 'debug';
+        // // Setup nock for simulating the web requests
+        // scope = nock(testURL)
+        //     .log(console.log)
+        //     // Login and logout
+        //     .post(authAPI, loginRequest)
+        //     .reply(200, { token: testToken, result: true, deviceStatus: 'IDLE', data: {}, errors: [] })
+        //     .post(authAPI, logoutRequest)
+        //     .reply(200, { result: true, deviceStatus: 'IDLE', data: {}, errors: [] })
+            
+        //     // Product list
+        //     .post(productsAPI, productsGetRequest)
+        //     .reply(200, productsGetResponse)
 
-            //config.native.dbtype   = 'sqlite';
+        //     // Scenes list
+        //     .post(scenesAPI, scenesGetRequest)
+        //     .reply(200, scenesGetResponse)
 
-            setup.setAdapterConfig(config.common, config.native);
+        //     // Make it permanent
+        //     .persist();
+        //     nock.emitter.on('no match', function(req) {
+        //         console.log(JSON.stringify(req));
+        //         });
 
-            setup.startController(true, function(id, obj) {}, function (id, state) {
-                    if (onStateChanged) onStateChanged(id, state);
+        // Setup mockserver for simulating the web requests
+        mockserver.start_mockserver({serverPort: 1080}).then(function () {
+            // Mocking authentication API
+            mockServerClient('localhost', 1080).mockAnyResponse({
+                "httpRequest": {
+                    "method": 'POST',
+                    "path": authAPI,
+                    "body": {
+                        "type": 'JSON',
+                        "value": JSON.stringify(loginRequest)
+                    }
                 },
-                function (_objects, _states) {
-                    objects = _objects;
-                    states  = _states;
-                    _done();
-                });
+                "httpResponse": {
+                    "statusCode": 200,
+                    "body": JSON.stringify({ token: testToken, result: true, deviceStatus: 'IDLE', data: {}, errors: [] })
+                },
+                "times": {
+                    "unlimited": true
+                }
+            });
+            mockServerClient('localhost', 1080).mockAnyResponse({
+                "httpRequest": {
+                    "method": 'POST',
+                    "path": authAPI,
+                    "body": {
+                        "type": 'JSON',
+                        "value": JSON.stringify(logoutRequest)
+                    }
+                },
+                "httpResponse": {
+                    "statusCode": 200,
+                    "body": JSON.stringify({ result: true, deviceStatus: 'IDLE', data: {}, errors: [] })
+                },
+                "times": {
+                    "unlimited": true
+                }
+            });
+
+            // Mocking products API
+            mockServerClient('localhost', 1080).mockAnyResponse({
+                "httpRequest": {
+                    "method": 'POST',
+                    "path": productsAPI,
+                    "body": {
+                        "type": 'JSON',
+                        "value": JSON.stringify(productsGetRequest)
+                    }
+                },
+                "httpResponse": {
+                    "statusCode": 200,
+                    "body": JSON.stringify(productsGetResponse)
+                },
+                "times": {
+                    "unlimited": true
+                }
+            });
+
+            // Mocking scenes API
+            mockServerClient('localhost', 1080).mockAnyResponse({
+                "httpRequest": {
+                    "method": 'POST',
+                    "path": scenesAPI,
+                    "body": {
+                        "type": 'JSON',
+                        "value": JSON.stringify(scenesGetRequest)
+                    }
+                },
+                "httpResponse": {
+                    "statusCode": 200,
+                    "body": JSON.stringify(scenesGetResponse)
+                },
+                "times": {
+                    "unlimited": true
+                }
+            });
+        
+
+            setup.setupController(function () {
+                var config = setup.getAdapterConfig();
+                // enable adapter
+                config.common.enabled  = true;
+                config.common.loglevel = 'debug';
+
+                //config.native.dbtype   = 'sqlite';
+                config.native.host = testURL;
+                config.native.password = testValidPW;
+
+                setup.setAdapterConfig(config.common, config.native);
+
+                setup.startController(true, function(id, obj) {}, function (id, state) {
+                        if (onStateChanged) onStateChanged(id, state);
+                    },
+                    function (_objects, _states) {
+                        objects = _objects;
+                        states  = _states;
+                        _done();
+                    });
+            });
         });
     });
 
@@ -118,7 +375,28 @@ describe('Test ' + adapterShortName + ' adapter', function() {
                 });
         });
     });
-/**/
+
+/*
+    WAIT for adapter to setup
+*/
+    it('Test ' + adapterShortName + ' adapter: Wait for adapter to initialize', function (done) {
+        this.timeout(30000);
+        setTimeout(function () {
+            done();
+        }, 10000);
+    });
+
+/*
+    Check number of products
+*/
+    it('Test ' + adapterShortName + ' adapter: Check number of products found', function (done) {
+        this.timeout(30000);
+        checkValueOfState('klf200.0.products.productsFound', 5, function (res) {
+            if (res) console.log(res);
+            expect(res, 'Cannot check number of products found').to.be.undefined;
+            done();
+        });
+    });
 
 /*
     PUT YOUR OWN TESTS HERE USING
@@ -136,5 +414,9 @@ describe('Test ' + adapterShortName + ' adapter', function() {
             console.log('Adapter normal terminated: ' + normalTerminated);
             done();
         });
+
+        // nock.cleanAll();
+        mockServerClient('localhost', 1080).reset();
+        mockserver.stop_mockserver();
     });
 });
