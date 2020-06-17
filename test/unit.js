b3a84a281e3384d288f3f244a988e6d8338dacee
Michael Schroeder <Michael.Schroeder@pmone.com>
2020-06-17 12:42:47 +0200

    Fix unit test for node v10

--- test/unit.js
+++ test/unit.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable prettier/prettier */
 const path = require("path");
@@ -14,62 +16,51 @@ should();
 use(sinonChai);
 use(chaiAsPromised);
 
-class MockConnect {
-	loginAsync = sinon.stub().resolves();
-	logoutAsync = sinon.stub().resolves();
-	sendFrameAsync = sinon.stub().resolves();
-	on = sinon.stub();
-	KLF200SocketProtocol = undefined;
-}
+class MockConnect {}
+MockConnect.prototype.loginAsync = sinon.stub().resolves();
+MockConnect.prototype.logoutAsync = sinon.stub().resolves();
+MockConnect.prototype.sendFrameAsync = sinon.stub().resolves();
+MockConnect.prototype.on = sinon.stub();
+MockConnect.prototype.KLF200SocketProtocol = undefined;
 
 const originalKLF200API = require("klf-200-api");
 
-class MockGateway {
-	getVersionAsync = sinon.stub().resolves({
-		SoftwareVersion: {
-			CommandVersion: 0,
-			MainVersion: 2,
-			SubVersion: 0,
-			BranchID: 0,
-			Build: 71,
-			MicroBuild: 0,
-			toString: sinon.stub().returns("0.2.0.0.71.0"),
-		},
-		HardwareVersion: 5,
-		ProductGroup: 14,
-		ProductType: 3,
-	});
+class MockGateway {}
+MockGateway.prototype.getVersionAsync = sinon.stub().resolves({
+	SoftwareVersion: {
+		CommandVersion: 0,
+		MainVersion: 2,
+		SubVersion: 0,
+		BranchID: 0,
+		Build: 71,
+		MicroBuild: 0,
+		toString: sinon.stub().returns("0.2.0.0.71.0"),
+	},
+	HardwareVersion: 5,
+	ProductGroup: 14,
+	ProductType: 3,
+});
+MockGateway.prototype.getProtocolVersionAsync = sinon.stub().resolves({ MajorVersion: 3, MinorVersion: 14 });
+MockGateway.prototype.getStateAsync = sinon.stub().resolves({
+	GatewayState: originalKLF200API.GatewayState.GatewayMode_WithActuatorNodes,
+	SubState: originalKLF200API.GatewaySubState.Idle,
+});
+MockGateway.prototype.enableHouseStatusMonitorAsync = sinon.stub().resolves();
+MockGateway.prototype.setUTCDateTimeAsync = sinon.stub().resolves();
+MockGateway.prototype.setTimeZoneAsync = sinon.stub().resolves();
 
-	getProtocolVersionAsync = sinon.stub().resolves({ MajorVersion: 3, MinorVersion: 14 });
-	getStateAsync = sinon
-		.stub()
-		.resolves({
-			GatewayState: originalKLF200API.GatewayState.GatewayMode_WithActuatorNodes,
-			SubState: originalKLF200API.GatewaySubState.Idle,
-		});
+class MockScenes {}
+MockScenes.createScenesAsync = sinon.stub().resolves({ Scenes: [], onRemovedScene: sinon.stub() });
 
-	enableHouseStatusMonitorAsync = sinon.stub().resolves();
+class MockGroups {}
+MockGroups.createGroupsAsync = sinon
+	.stub()
+	.resolves({ Groups: [], onRemovedGroup: sinon.stub(), onChangedGroup: sinon.stub() });
 
-	setUTCDateTimeAsync = sinon.stub().resolves();
-
-	setTimeZoneAsync = sinon.stub().resolves();
-}
-
-class MockScenes {
-	static createScenesAsync = sinon.stub().resolves({ Scenes: [], onRemovedScene: sinon.stub() });
-}
-
-class MockGroups {
-	static createGroupsAsync = sinon
-		.stub()
-		.resolves({ Groups: [], onRemovedGroup: sinon.stub(), onChangedGroup: sinon.stub() });
-}
-
-class MockProducts {
-	static createProductsAsync = sinon
-		.stub()
-		.resolves({ Products: [], onRemovedProduct: sinon.stub(), onNewProduct: sinon.stub() });
-}
+class MockProducts {}
+MockProducts.createProductsAsync = sinon
+	.stub()
+	.resolves({ Products: [], onRemovedProduct: sinon.stub(), onNewProduct: sinon.stub() });
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.unit(path.join(__dirname, ".."), {
