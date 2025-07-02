21c9f580a7f54ceb9bf8f3d369d4308ff14e1d83
Michael Schröder <mschroeder@pmone.com>
2025-07-02 15:00:17 +0200

    Update dependencies

--- tasks.ts
+++ tasks.ts
@@ -4,17 +4,17 @@
  * MIT License
  *
  */
-import { copyFiles, deleteFoldersRecursive, npmInstall, buildReact } from "@iobroker/build-tools";
+import { buildReact, copyFiles, deleteFoldersRecursive, npmInstall } from "@iobroker/build-tools";
 
 const __dirname = import.meta.dirname;
 const srcAdmin = `${__dirname}/src-admin/`;
 
-function clean() {
+function clean(): void {
 	deleteFoldersRecursive(`${__dirname}/admin/custom`);
 	deleteFoldersRecursive(`${__dirname}/src-admin/build`);
 }
 
-function copy() {
+function copy(): void {
 	copyFiles(["src-admin/build/assets/*.js", "!src-admin/build/static/js/vendors*.js"], "admin/custom/assets");
 	copyFiles(["src-admin/build/assets/*.map", "!src-admin/build/static/js/vendors*.map"], "admin/custom/assets");
 	copyFiles(["src-admin/build/customComponents.js"], "admin/custom");
@@ -25,16 +25,18 @@ function copy() {
 if (process.argv.find(arg => arg === "--0-clean")) {
 	clean();
 } else if (process.argv.find(arg => arg === "--1-npm")) {
-	npmInstall(srcAdmin).catch(e => console.error(`Cannot install packages: ${e.toString()}`));
+	npmInstall(srcAdmin, { clean: true, force: false }).catch((e: Error) =>
+		console.error(`Cannot install packages: ${e.toString()}`),
+	);
 } else if (process.argv.find(arg => arg === "--2-compile")) {
 	buildReact(srcAdmin, { rootDir: srcAdmin, vite: true }).catch(e =>
-		console.error(`Cannot install packages: ${e.toString()}`),
+		console.error(`Cannot build React: ${e.toString()}`),
 	);
 } else if (process.argv.find(arg => arg === "--3-copy")) {
 	copy();
 } else if (process.argv.includes("--default")) {
 	clean();
-	npmInstall(srcAdmin)
+	npmInstall(srcAdmin, { clean: true, force: false })
 		.then(() => buildReact(srcAdmin, { rootDir: srcAdmin, vite: true }))
 		.then(() => copy())
 		.catch(e => {
