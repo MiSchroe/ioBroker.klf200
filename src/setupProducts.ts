835fed37ddb8225d7c48291356fda4adcc88f0e5
Michael Schroeder <michael.schroeder@gmx.de>
2026-08-14 16:27:54 +0200

    Fix type issues

--- src/setupProducts.ts
+++ src/setupProducts.ts
@@ -834,7 +834,7 @@ export class SetupProducts {
 
 				let limitationTime = NaN;
 				try {
-					limitationTime = product.getLimitationTime(parameter);
+					limitationTime = product.getLimitationTime(parameter) ?? NaN;
 				} catch (error) {
 					if (error instanceof Error && error.message === "Lock time value out of range.") {
 						limitationTime = NaN;
@@ -1114,7 +1114,7 @@ export class SetupProducts {
 
 					let limitationTime = NaN;
 					try {
-						limitationTime = Math.round(product.getLimitationTime(parameter) * 100);
+						limitationTime = Math.round((product.getLimitationTime(parameter) ?? NaN) * 100);
 					} catch (error) {
 						if (error instanceof Error && error.message === "Lock time value out of range.") {
 							limitationTime = NaN;
@@ -1327,3 +1327,4 @@ export class SetupProducts {
 		disposalMap.set(refreshLimitationStateId, refreshLimitationListener);
 	}
 }
+
